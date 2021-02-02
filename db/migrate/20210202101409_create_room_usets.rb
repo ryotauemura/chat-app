7d735608197061bfcbebbf2fa5c3dd7767b427e5
ryotauemura <taurin276@gmail.com>
2021-02-02 19:22:17 +0900

    room_usersテーブルの作成

---
+++ db/migrate/20210202101409_create_room_usets.rb
@@ -0,0 +1,9 @@
+class CreateRoomUsets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :room_usets do |t|
+      t.references :room, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_100450) do
+ActiveRecord::Schema.define(version: 2021_02_02_101409) do
+
+  create_table "room_usets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_usets_on_room_id"
+    t.index ["user_id"], name: "index_room_usets_on_user_id"
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_02_02_100450) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "room_usets", "rooms"
+  add_foreign_key "room_usets", "users"
 end
